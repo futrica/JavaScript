aa79a2123c059bb75b1511aac1b63eab88ececc4
futrica <futrica@futrica-pc.(none)>
2012-06-30 18:17:18 -0300

    modificando o arquivo .jse treinando outros comandos - proximo passo: looping e criacao de html

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -17,10 +17,61 @@
         - etc .
         
 __________________________ Treinamento JavaScript __________________________ */
-         
 
-img_aparece = new Array();
-link_clicado = new Array();
+
+         
+list =  document.getElementById('wrapper');
+imgs = list.getElementsByTagName('img');  
+//links = list.getElementsByTagName('a')
+
+//imgs[2].style.opacity = 1;
+//document.write(links[1].text)
+
+function troca(num) {	
+	if (num == 0) {
+		imgs[0].style.opacity = 1;
+		imgs[1].style.opacity = 0;
+		imgs[2].style.opacity = 0;
+		imgs[3].style.opacity = 0;
+		imgs[4].style.opacity = 0;
+			} else  if (num == 1) {
+		imgs[0].style.opacity = 0;
+		imgs[1].style.opacity = 1;
+		imgs[2].style.opacity = 0;
+		imgs[3].style.opacity = 0;
+		imgs[4].style.opacity = 0;
+			} else  if (num == 2) {
+		imgs[0].style.opacity = 0;
+		imgs[1].style.opacity = 0;
+		imgs[2].style.opacity = 1;
+		imgs[3].style.opacity = 0;
+		imgs[4].style.opacity = 0;
+			} else  if (num == 3) {
+		imgs[0].style.opacity = 0;
+		imgs[1].style.opacity = 0;
+		imgs[2].style.opacity = 0;
+		imgs[3].style.opacity = 1;
+		imgs[4].style.opacity = 0;
+			} else  if (num == 4) {
+		imgs[0].style.opacity = 0;
+		imgs[1].style.opacity = 0;
+		imgs[2].style.opacity = 0;
+		imgs[3].style.opacity = 0;
+		imgs[4].style.opacity = 1;
+			}
+}
+
+
+
+
+
+
+
+
+/* --------------------------------------------------
+		INATIVO
+-----------------------------------------------------
+
 
 img_aparece[0] = document.getElementById("img_1").style;
 img_aparece[1] = document.getElementById("img_2").style;
@@ -28,14 +79,11 @@ img_aparece[2] = document.getElementById("img_3").style;
 img_aparece[3] = document.getElementById("img_4").style;
 img_aparece[4] = document.getElementById("img_5").style;
 
-
-
 link_clicado[0] = document.getElementById("link_1").text;
 link_clicado[1] = document.getElementById("link_2").text;
 link_clicado[2] = document.getElementById("link_3").text;
 link_clicado[3] = document.getElementById("link_4").text;
 link_clicado[4] = document.getElementById("link_5").text;
-
 
 
 function executaOnclick0(){ 
@@ -77,3 +125,5 @@ function executaOnclick4(){
      img_aparece[3].opacity = 0;
      img_aparece[4].opacity = 1;
 }
+
+*/
